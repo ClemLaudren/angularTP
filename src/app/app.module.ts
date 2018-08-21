a57ad06380704cd7aa6fa9f64a22100ede56fd3f
Clemence Laudren <clem.laudren@gmail.com>
2018-08-21 14:20:23 +0200

    add article service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {ListComponent} from './list/list.component';
-import {ArticleComponent} from './list/article/article.component';
+import { ListComponent } from './list/list.component';
+import { ArticleComponent } from './list/article/article.component';
+
+import { ArticleService } from './services/article/article.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import {ArticleComponent} from './list/article/article.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    ArticleService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
